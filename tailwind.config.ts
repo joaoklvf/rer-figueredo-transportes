75dd6e4466765735f2c5c13f56b2b66f1b411fce
João Figueredo <joaoklvf@gmail.com>
2025-10-02 13:10:16 -0300

    feat: using new lint

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from 'tailwindcss';
+import tailwindForms from '@tailwindcss/forms';
 
 const config: Config = {
   content: [
@@ -27,6 +28,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [require('@tailwindcss/forms')],
+  plugins: [tailwindForms],
 };
 export default config;
