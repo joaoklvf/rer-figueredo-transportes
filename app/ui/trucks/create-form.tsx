4cca3cc2b255c4c83d2496fbe42f8711106beaa8
João <joaoklvf@gmail.com>
2025-10-03 11:50:30 -0300

    fix: cancel button name

--- app/ui/trucks/create-form.tsx
+++ app/ui/trucks/create-form.tsx
@@ -110,7 +110,7 @@ export default function Form() {
           href="/dashboard/caminhoes"
           className="flex h-10 items-center rounded-lg bg-gray-100 px-4 text-sm font-medium text-gray-600 transition-colors hover:bg-gray-200"
         >
-          Cancel
+          Cancelar
         </Link>
         <Button type="submit">Cadastrar</Button>
       </div>
