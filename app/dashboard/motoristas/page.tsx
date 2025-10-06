b80c2c313cd34e8d0b4f1319f6288e95622166a2
João Figueredo <joaoklvf@gmail.com>
2025-10-06 20:26:55 -0300

    fix: pesquisar motoristas

--- app/dashboard/motoristas/page.tsx
+++ app/dashboard/motoristas/page.tsx
@@ -28,7 +28,7 @@ export default async function Page(props: Readonly<{
         <h1 className={`${lusitana.className} text-2xl`}>Motoristas</h1>
       </div>
       <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
-        <Search placeholder="Pesquisar caminhões..." />
+        <Search placeholder="Pesquisar motoristas..." />
         <CreateDriver />
       </div>
       <Suspense key={query + currentPage} fallback={<DriversTableSkeleton />}>
