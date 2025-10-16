d189c3f3256a9c8c595cc732d5d691e66ffbde8f
João Figueredo <joaoklvf@gmail.com>
2025-10-16 01:18:52 -0300

    feat: skeleton

--- app/ui/trips/skeletons/trips.skeletons.tsx
+++ app/ui/trips/skeletons/trips.skeletons.tsx
@@ -41,19 +41,16 @@ export function TripsTableSkeleton() {
             <thead className="rounded-lg text-left text-sm font-normal">
               <tr>
                 <th scope="col" className="px-4 py-5 font-medium sm:pl-6">
-                  Customer
+                  Motorista
                 </th>
                 <th scope="col" className="px-3 py-5 font-medium">
-                  Email
+                  Origem
                 </th>
                 <th scope="col" className="px-3 py-5 font-medium">
-                  Amount
+                  Destino
                 </th>
                 <th scope="col" className="px-3 py-5 font-medium">
-                  Date
-                </th>
-                <th scope="col" className="px-3 py-5 font-medium">
-                  Status
+                  Caminhão
                 </th>
                 <th
                   scope="col"
